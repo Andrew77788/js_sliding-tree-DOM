7cfd6e553eeec8f440fb0c0f262252ac0073afdc
Andrii Dotsulenko <58427426+Andrew77788@users.noreply.github.com>
2024-11-06 16:23:49 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function() {
+    const listItems = document.querySelectorAll(".tree li");
+    listItems.forEach((li)=>{
+        const text = li.firstChild.nodeValue.trim();
+        if (li.childElementCount !== 0) {
+            const span = document.createElement("span");
+            span.textContent = text;
+            li.firstChild.remove();
+            li.prepend(span);
+        }
+    });
+    document.querySelectorAll("ul").forEach((i)=>{
+        i.style.display = "block";
+    });
+    const spans = document.querySelectorAll("span");
+    spans.forEach((span)=>{
+        span.addEventListener("click", ()=>{
+            const content = span.nextElementSibling;
+            if (content.style.display === "" || content.style.display === "none") content.style.display = "block";
+            else content.style.display = "none";
+        });
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
